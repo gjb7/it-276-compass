1021f28c3b2f931d662eb596069f79f0182408f3
Grant J. Butler <butler.grant.j@gmail.com>
2014-12-03 19:23:13 -0500

    Add CMPMap.

---
+++ Compass/Classes/Model/CMPMap.h
@@ -0,0 +1,13 @@
+//
+//  CMPMap.h
+//  Compass
+//
+//  Created by Grant Butler on 12/3/14.
+//  Copyright (c) 2014 Grant Butler. All rights reserved.
+//
+
+@import Foundation;
+
+@interface CMPMap : NSObject
+
+@end
---
+++ Compass/Classes/Model/CMPMap.m
@@ -0,0 +1,13 @@
+//
+//  CMPMap.m
+//  Compass
+//
+//  Created by Grant Butler on 12/3/14.
+//  Copyright (c) 2014 Grant Butler. All rights reserved.
+//
+
+#import "CMPMap.h"
+
+@implementation CMPMap
+
+@end
