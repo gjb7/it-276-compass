05d9797341d05a7d94b18ef90813e91e991af7fa
Grant J. Butler <butler.grant.j@gmail.com>
2014-12-03 23:41:28 -0500

    Add CMPLayer.

---
+++ Compass/Classes/Model/CMPLayer.h
@@ -0,0 +1,15 @@
+//
+//  CMPLayer.h
+//  Compass
+//
+//  Created by Grant Butler on 12/3/14.
+//  Copyright (c) 2014 Grant Butler. All rights reserved.
+//
+
+@import Foundation;
+
+@interface CMPLayer : NSObject
+
+@property (nonatomic) NSMutableArray *tiles;
+
+@end
