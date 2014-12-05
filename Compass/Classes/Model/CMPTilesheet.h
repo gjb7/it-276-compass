4e53a0df700342c5c5fab48451272a1f467e6e89
Grant J. Butler <butler.grant.j@gmail.com>
2014-12-05 08:37:49 -0500

    Add CMPTilesheet.

---
+++ Compass/Classes/Model/CMPTilesheet.h
@@ -0,0 +1,15 @@
+//
+//  CMPTilesheet.h
+//  Compass
+//
+//  Created by Grant Butler on 12/5/14.
+//  Copyright (c) 2014 Grant Butler. All rights reserved.
+//
+
+@import UIKit;
+
+@interface CMPTilesheet : NSObject
+
+@property (nonatomic) UIImage *sprite;
+
+@end
---
+++ Compass/Classes/Model/CMPTilesheet.m
@@ -0,0 +1,13 @@
+//
+//  CMPTilesheet.m
+//  Compass
+//
+//  Created by Grant Butler on 12/5/14.
+//  Copyright (c) 2014 Grant Butler. All rights reserved.
+//
+
+#import "CMPTilesheet.h"
+
+@implementation CMPTilesheet
+
+@end
