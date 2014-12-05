82bd57d1a77f2ef7c33a28e4a3db620268acfe19
Grant J. Butler <butler.grant.j@gmail.com>
2014-12-05 09:01:21 -0500

    Add CMPMapParser.

---
+++ Compass/Classes/Controller/Parsing/CMPMapParser.h
@@ -0,0 +1,13 @@
+//
+//  CMPMapParser.h
+//  Compass
+//
+//  Created by Grant Butler on 12/5/14.
+//  Copyright (c) 2014 Grant Butler. All rights reserved.
+//
+
+@import Foundation;
+
+@interface CMPMapParser : NSObject
+
+@end
---
+++ Compass/Classes/Controller/Parsing/CMPMapParser.m
@@ -0,0 +1,14 @@
+
+//
+//  CMPMapParser.m
+//  Compass
+//
+//  Created by Grant Butler on 12/5/14.
+//  Copyright (c) 2014 Grant Butler. All rights reserved.
+//
+
+#import "CMPMapParser.h"
+
+@implementation CMPMapParser
+
+@end
