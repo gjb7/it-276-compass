9592a4421865e1bf716bb835b64310347ebb4755
Grant J. Butler <butler.grant.j@gmail.com>
2014-12-05 18:00:30 -0500

    Add CMPV1MapParser.

---
+++ Compass/Classes/Controller/Parsing/CMPV1MapParser.h
@@ -0,0 +1,13 @@
+//
+//  CMPV1MapParser.h
+//  Compass
+//
+//  Created by Grant Butler on 12/5/14.
+//  Copyright (c) 2014 Grant Butler. All rights reserved.
+//
+
+#import "CMPMapParser.h"
+
+@interface CMPV1MapParser : CMPMapParser
+
+@end
---
+++ Compass/Classes/Controller/Parsing/CMPV1MapParser.m
@@ -0,0 +1,13 @@
+//
+//  CMPV1MapParser.m
+//  Compass
+//
+//  Created by Grant Butler on 12/5/14.
+//  Copyright (c) 2014 Grant Butler. All rights reserved.
+//
+
+#import "CMPV1MapParser.h"
+
+@implementation CMPV1MapParser
+
+@end
