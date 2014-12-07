ac511a069fd38fe4c6b7ad229d7e26a7589162a1
Grant J. Butler <butler.grant.j@gmail.com>
2014-12-07 13:30:29 -0500

    Append map size to buffer.

--- Compass/Classes/Controller/CMPV1MapSerializer.m
+++ Compass/Classes/Controller/CMPV1MapSerializer.m
@@ -16,6 +16,7 @@
     NSMutableData *buffer = [[NSMutableData alloc] init];
     
     [self appendMapVersionToBuffer:buffer];
+    [self appendMapSize:map.size toBuffer:buffer];
     
     return buffer;
 }
@@ -23,6 +24,14 @@
 - (void)appendMapVersionToBuffer:(NSMutableData *)buffer {
     char versionBuffer[3] = { 'V', 1, ';' };
     [buffer appendBytes:versionBuffer length:3];
+}
+
+- (void)appendMapSize:(CGSize)size toBuffer:(NSMutableData *)buffer {
+    char widthBuffer[3] = { 'W', (uint8_t)size.width, ';' };
+    [buffer appendBytes:widthBuffer length:3];
+    
+    char heightBuffer[3] = { 'H', (uint8_t)size.height, ';' };
+    [buffer appendBytes:heightBuffer length:3];
 }
 
 @end
