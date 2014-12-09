33f02e4545fe35ff5e6083c999aa69455e0a0b41
Grant Butler <gjb7@njit.edu>
2014-12-08 19:43:05 -0500

    Add CMPProjectViewController.

---
+++ Compass/Classes/Controller/View Controller/CMPProjectViewController.m
@@ -0,0 +1,27 @@
+//
+//  CMPProjectViewController.m
+//  Compass
+//
+//  Created by ACM Member on 12/8/14.
+//  Copyright (c) 2014 Grant Butler. All rights reserved.
+//
+
+#import "CMPProjectViewController.h"
+
+@interface CMPProjectViewController ()
+
+@end
+
+@implementation CMPProjectViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
