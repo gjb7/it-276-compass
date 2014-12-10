fc8984ea7208c9310b0fe5e44901bed86c877fe4
Grant J. Butler <butler.grant.j@gmail.com>
2014-12-09 19:13:42 -0500

    Add CMPMapView.

---
+++ Compass/Classes/View/CMPMapView.h
@@ -0,0 +1,13 @@
+//
+//  CMPMapView.h
+//  Compass
+//
+//  Created by Grant Butler on 12/9/14.
+//  Copyright (c) 2014 Grant Butler. All rights reserved.
+//
+
+@import UIKit;
+
+@interface CMPMapView : UIView
+
+@end
---
+++ Compass/Classes/View/CMPMapView.m
@@ -0,0 +1,13 @@
+//
+//  CMPMapView.m
+//  Compass
+//
+//  Created by Grant Butler on 12/9/14.
+//  Copyright (c) 2014 Grant Butler. All rights reserved.
+//
+
+#import "CMPMapView.h"
+
+@implementation CMPMapView
+
+@end
