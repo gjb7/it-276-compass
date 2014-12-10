c0b49cb58c7dc6369dad8491e0e2d548e5937581
Grant J. Butler <butler.grant.j@gmail.com>
2014-12-09 19:13:47 -0500

    Add CMPLayerView.

---
+++ Compass/Classes/View/CMPLayerView.h
@@ -0,0 +1,13 @@
+//
+//  CMPLayerView.h
+//  Compass
+//
+//  Created by Grant Butler on 12/9/14.
+//  Copyright (c) 2014 Grant Butler. All rights reserved.
+//
+
+@import UIKit;
+
+@interface CMPLayerView : UIView
+
+@end
---
+++ Compass/Classes/View/CMPLayerView.m
@@ -0,0 +1,13 @@
+//
+//  CMPLayerView.m
+//  Compass
+//
+//  Created by Grant Butler on 12/9/14.
+//  Copyright (c) 2014 Grant Butler. All rights reserved.
+//
+
+#import "CMPLayerView.h"
+
+@implementation CMPLayerView
+
+@end
