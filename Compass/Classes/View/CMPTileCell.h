ab0a44fbaa79a1fe7cf284a9e6fb62ec6743a91a
Grant J. Butler <butler.grant.j@gmail.com>
2014-12-11 08:48:38 -0500

    Add CMPTileCell.

---
+++ Compass/Classes/View/CMPTileCell.h
@@ -0,0 +1,13 @@
+//
+//  CMPTileCell.h
+//  Compass
+//
+//  Created by Grant Butler on 12/11/14.
+//  Copyright (c) 2014 Grant Butler. All rights reserved.
+//
+
+@import UIKit;
+
+@interface CMPTileCell : UICollectionViewCell
+
+@end
---
+++ Compass/Classes/View/CMPTileCell.m
@@ -0,0 +1,13 @@
+//
+//  CMPTileCell.m
+//  Compass
+//
+//  Created by Grant Butler on 12/11/14.
+//  Copyright (c) 2014 Grant Butler. All rights reserved.
+//
+
+#import "CMPTileCell.h"
+
+@implementation CMPTileCell
+
+@end
