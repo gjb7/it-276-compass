e8f9d30ca19028358ce9c6647af7462d2fa22df7
Grant J. Butler <butler.grant.j@gmail.com>
2014-12-14 18:35:28 -0500

    Add CMPMapThumbnailManager.

---
+++ Compass/Classes/Controller/CMPMapThumbnailManager.h
@@ -0,0 +1,13 @@
+//
+//  CMPMapThumbnailManager.h
+//  Compass
+//
+//  Created by Grant Butler on 12/14/14.
+//  Copyright (c) 2014 Grant Butler. All rights reserved.
+//
+
+@import Foundation;
+
+@interface CMPMapThumbnailManager : NSObject
+
+@end
---
+++ Compass/Classes/Controller/CMPMapThumbnailManager.m
@@ -0,0 +1,13 @@
+//
+//  CMPMapThumbnailManager.m
+//  Compass
+//
+//  Created by Grant Butler on 12/14/14.
+//  Copyright (c) 2014 Grant Butler. All rights reserved.
+//
+
+#import "CMPMapThumbnailManager.h"
+
+@implementation CMPMapThumbnailManager
+
+@end
