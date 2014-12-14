4af7c12e5e7b5df6b32b044ab59ce7d8c27fef89
Grant J. Butler <butler.grant.j@gmail.com>
2014-12-14 18:32:51 -0500

    Add CMPLevelIconView.

---
+++ Compass/Classes/View/CMPLevelIconView.h
@@ -0,0 +1,13 @@
+//
+//  CMPLevelIconView.h
+//  Compass
+//
+//  Created by Grant Butler on 12/14/14.
+//  Copyright (c) 2014 Grant Butler. All rights reserved.
+//
+
+@import UIKit;
+
+@interface CMPLevelIconView : UICollectionViewCell
+
+@end
---
+++ Compass/Classes/View/CMPLevelIconView.m
@@ -0,0 +1,13 @@
+//
+//  CMPLevelIconView.m
+//  Compass
+//
+//  Created by Grant Butler on 12/14/14.
+//  Copyright (c) 2014 Grant Butler. All rights reserved.
+//
+
+#import "CMPLevelIconView.h"
+
+@implementation CMPLevelIconView
+
+@end
