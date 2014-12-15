40b813dfa67c5f6b4c00bb49198463b6e02569d3
Grant J. Butler <butler.grant.j@gmail.com>
2014-12-14 22:35:34 -0500

    Add CMPSplitViewController.

---
+++ Compass/Classes/Controller/View Controller/CMPSplitViewController.h
@@ -0,0 +1,14 @@
+//
+//  CMPSplitViewController.h
+//  Compass
+//
+//  Created by Grant Butler on 12/14/14.
+//  Copyright (c) 2014 Grant Butler. All rights reserved.
+//
+
+@import UIKit;
+
+// We need our own implementation of a split view controller as UISplitViewController is not allowed to be used inside a navigation controller.
+@interface CMPSplitViewController : UIViewController
+
+@end
---
+++ Compass/Classes/Controller/View Controller/CMPSplitViewController.m
@@ -0,0 +1,13 @@
+//
+//  CMPSplitViewController.m
+//  Compass
+//
+//  Created by Grant Butler on 12/14/14.
+//  Copyright (c) 2014 Grant Butler. All rights reserved.
+//
+
+#import "CMPSplitViewController.h"
+
+@implementation CMPSplitViewController
+
+@end
