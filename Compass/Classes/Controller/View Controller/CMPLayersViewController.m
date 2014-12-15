a7cc18c3584b70755483f22eefa8c44b0ec4d2f7
Grant J. Butler <butler.grant.j@gmail.com>
2014-12-14 23:39:08 -0500

    Add CMPLayersViewController.

---
+++ Compass/Classes/Controller/View Controller/CMPLayersViewController.m
@@ -0,0 +1,15 @@
+//
+//  CMPLayersViewController.m
+//  Compass
+//
+//  Created by Grant Butler on 12/14/14.
+//  Copyright (c) 2014 Grant Butler. All rights reserved.
+//
+
+#import "CMPLayersViewController.h"
+
+@implementation CMPLayersViewController
+
+
+
+@end
