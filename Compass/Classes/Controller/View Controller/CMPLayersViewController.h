d2e31c5a7ebf7e8472d33ce0f414b51af464a87e
Grant J. Butler <butler.grant.j@gmail.com>
2014-12-14 23:49:38 -0500

    Add some implementation.

--- Compass/Classes/Controller/View Controller/CMPLayersViewController.h
+++ Compass/Classes/Controller/View Controller/CMPLayersViewController.h
@@ -8,6 +8,10 @@
 
 @import UIKit;
 
+@class CMPMap;
+
 @interface CMPLayersViewController : UITableViewController
+
+@property (nonatomic) CMPMap *map;
 
 @end
--- Compass/Classes/Controller/View Controller/CMPLayersViewController.m
+++ Compass/Classes/Controller/View Controller/CMPLayersViewController.m
@@ -8,8 +8,24 @@
 
 #import "CMPLayersViewController.h"
 
+#import "CMPMap.h"
+
 @implementation CMPLayersViewController
 
+- (void)setMap:(CMPMap *)map {
+    _map = map;
+    
+    if (self.isViewLoaded) {
+        [self.tableView reloadData];
+    }
+}
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.map.layers.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return nil;
+}
 
 @end
