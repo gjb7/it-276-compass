11b708be17142f2ec3d234806ec434a7ffdc82a2
Grant J. Butler <butler.grant.j@gmail.com>
2014-12-14 19:45:39 -0500

    Make property mutable.

--- Compass/Classes/Controller/View Controller/CMPLevelsViewController.m
+++ Compass/Classes/Controller/View Controller/CMPLevelsViewController.m
@@ -19,7 +19,7 @@ static NSString * const CMPShowEditorSegueIdentifier = @"CMPShowEditorSegue";
 
 @interface CMPLevelsViewController ()
 
-@property (nonatomic) NSArray *levels;
+@property (nonatomic) NSMutableArray *levels;
 
 @end
 
