48dc2670bfce073120accc3ed679c92c45d7385e
Grant J. Butler <butler.grant.j@gmail.com>
2014-12-17 11:23:38 -0500

    Add CMPEditorTileManager.

---
+++ Compass/Classes/Controller/CMPEditorTileManager.h
@@ -0,0 +1,13 @@
+//
+//  CMPEditorTileManager.h
+//  Compass
+//
+//  Created by Grant Butler on 12/17/14.
+//  Copyright (c) 2014 Grant Butler. All rights reserved.
+//
+
+@import Foundation;
+
+@interface CMPEditorTileManager : NSObject
+
+@end
---
+++ Compass/Classes/Controller/CMPEditorTileManager.m
@@ -0,0 +1,13 @@
+//
+//  CMPEditorTileManager.m
+//  Compass
+//
+//  Created by Grant Butler on 12/17/14.
+//  Copyright (c) 2014 Grant Butler. All rights reserved.
+//
+
+#import "CMPEditorTileManager.h"
+
+@implementation CMPEditorTileManager
+
+@end
