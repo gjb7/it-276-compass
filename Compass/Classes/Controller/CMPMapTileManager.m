7509462336c669825fef03f5730bad63a0f3ef1f
Grant J. Butler <butler.grant.j@gmail.com>
2014-12-17 11:14:44 -0500

    Add CMPTileManager.

---
+++ Compass/Classes/Controller/CMPMapTileManager.m
@@ -0,0 +1,13 @@
+//
+//  CMPMapTileManager.m
+//  Compass
+//
+//  Created by Grant Butler on 12/16/14.
+//  Copyright (c) 2014 Grant Butler. All rights reserved.
+//
+
+#import "CMPMapTileManager.h"
+
+@implementation CMPMapTileManager
+
+@end
